e21369bbb3530efa3803cd8abcb304d9a0a9526d
DrSuperMario <mario@pythonslack.com>
2020-11-01 21:39:43 +0200

    updated with asyncio

--- scraper.py
+++ scraper.py
@@ -1,57 +1,84 @@
-import time
+import asyncio
 from datetime import datetime
 
-import pandas as pd
 
+from modules.dataCollector import Connect
+from connection.var import *
 from db import createDb
 from smtp import send_email
-from connection.var import *
-from modules.dataCollector import Connect
+
 
 TIME_LOOP = True
-time_passage = 1
+time_passage = 12000
 date_as = str(datetime.now())
+passwd = '<BLANK>'
+
+
+async def cryptoConnection(delay):
+
+    conn = Connect.crypto(url=URL[1],header=HEADERS['agent_desktop'])
+
+    cryptoConn, dbName = createDb("CryptoTable")  
+    conn.to_sql(dbName, cryptoConn, if_exists='append')
+    try:
+
+        send_email(messages='Information Collected from Crypto Source',
+                     subject=date_as, password=passwd)
+    
+    except EnvironmentError:
+        print("Error occured with SMTP authentication")
+
+    await asyncio.sleep(delay)
+
+
+async def newsConnection(delay):
+
+    conn = Connect.news(url=URL[4], header=HEADERS['agent_smartphone'])
+    newsConn, dbName = createDb("newsTable")  
+    conn.to_sql(dbName, newsConn, if_exists='append')
+    try:
+
+        send_email(messages='Information Collected from News sources', 
+                    subject=date_as, password=passwd)
+
+    except EnvironmentError:
+        print("Error occured with SMTP authentication")
+
+        await asyncio.sleep(delay)
+
+async def forexConnection(delay):
+
+    #Forex connection
+    conn = Connect.forex(url=URL[6], header=HEADERS['agent_desktop'])
+    forexConn, dbName = createDb("forexTable")  
+    conn.to_sql(dbName, forexConn, if_exists='append')
+    try:
+
+        send_email(messages='Information Collected from News sources', 
+                    subject=date_as, password=passwd)
+
+    except EnvironmentError:
+        print("Error occured with SMTP authentication")
+
+        await asyncio.sleep(delay)
+
+async def main():
+
+    await cryptoConnection(time_passage)
+    await newsConnection(time_passage)
+    await forexConnection(3600)
+
 
 if __name__=="__main__":
 
     while True:
-        try:
-            #Crypto Connection
-            cryptoConnection = Connect.crypto(url=URL[1],header=HEADERS['agent_desktop'])
-            cryptoConn, dbName = createDb("CryptoTable")  
-            cryptoConnection.to_sql(dbName, cryptoConn, if_exists='append')
-            time.sleep(time_passage)
-            send_email(messages='Information Collected Crypto', subject=date_as, password='<BLANK>)
-            
-            #news connection
-            newsConnection = Connect.news(url=URL[4], header=HEADERS['agent_smartphone'])
-            newsConn, dbName = createDb("newsTable")  
-            newsConnection.to_sql(dbName, newsConn, if_exists='append')
-            time.sleep(time_passage)
-            send_email(messages='Information Collected news', subject=date_as, password='<BKANK>')
 
-            #Forex connection
-            forexConnection = Connect.forex(url=URL[6], header=HEADERS['agent_desktop'])
-            forexConn, dbName = createDb("forexTable")  
-            forexConnection.to_sql(dbName, forexConn, if_exists='append')
-            time.sleep(time_passage)
-            #with open(f"saved/{datetime.strftime(datetime.now(), '%m-%d-%Y')}_CRYPTO.csv",'w+') as f:
-            #    f.write(df.to_csv(index=True))
-            #Remove password :D
-            
-            send_email(messages='Information Collected forex', subject=date_as, password='<BLANK>')
-            time.sleep(time_passage)
+        try:
+
+            asyncio.run(main())
 
         except KeyboardInterrupt:
             #print(f'Run canceled on {datetime.now()}')
             TIME_LOOP = False
-            cryptoConn.close()
-            try:
-                newsConn.close()
-                cryptoConn.close()
-            except NameError:
-                print("Not initialized yet")
-            break
-
-
-
+            send_email(messages='Program finished', 
+                    subject=date_as, password=passwd)
--- test.py
+++ test.py
@@ -1,18 +1,83 @@
+import asyncio
+from datetime import datetime
+
+
 from modules.dataCollector import Connect
 from connection.var import *
 from db import createDb
 from smtp import send_email
 
-#newConnection = Connect.news(url=URL[4], header=HEADERS['agent_smartphone'])
-"""
-newConnection = Connect.crypto(url=URL[1],header=HEADERS['agent_desktop'])
-#secondConnection = Connect.news(url=URL[4], header=HEADERS['agent_smartphone'])
-#thirdConnection = Connect.forex(url=URL[6], header=HEADERS['agent_desktop'])
 
-print(newConnection)
-#print(secondConnection)
-#print(thirdConnection)
-newsConn, dbName = createDb("newsTable")  
-newConnection.to_sql(dbName, newsConn, if_exists='append')
-newsConn.close()"""
-send_email(messages='Information Collected Crypto', subject="asd", password='Haxx0r001')
+TIME_LOOP = True
+time_passage = 12000
+date_as = str(datetime.now())
+passwd = '<BLANK>'
+
+
+async def cryptoConnection(delay):
+
+    conn = Connect.crypto(url=URL[1],header=HEADERS['agent_desktop'])
+
+    cryptoConn, dbName = createDb("CryptoTable")  
+    conn.to_sql(dbName, cryptoConn, if_exists='append')
+    try:
+
+        send_email(messages='Information Collected from Crypto Source',
+                     subject=date_as, password=passwd)
+    
+    except EnvironmentError:
+        print("Error occured with SMTP authentication")
+
+    await asyncio.sleep(delay)
+
+
+async def newsConnection(delay):
+
+    conn = Connect.news(url=URL[4], header=HEADERS['agent_smartphone'])
+    newsConn, dbName = createDb("newsTable")  
+    conn.to_sql(dbName, newsConn, if_exists='append')
+    try:
+
+        send_email(messages='Information Collected from News sources', 
+                    subject=date_as, password=passwd)
+
+    except EnvironmentError:
+        print("Error occured with SMTP authentication")
+
+        await asyncio.sleep(delay)
+
+async def forexConnection(delay):
+
+    #Forex connection
+    conn = Connect.forex(url=URL[6], header=HEADERS['agent_desktop'])
+    forexConn, dbName = createDb("forexTable")  
+    conn.to_sql(dbName, forexConn, if_exists='append')
+    try:
+
+        send_email(messages='Information Collected from News sources', 
+                    subject=date_as, password=passwd)
+
+    except EnvironmentError:
+        print("Error occured with SMTP authentication")
+
+        await asyncio.sleep(delay)
+
+async def main():
+
+    await cryptoConnection(time_passage)
+    await newsConnection(time_passage)
+    await forexConnection(3600)
+
+
+if __name__=="__main__":
+
+    while True:
+
+        try:
+
+            asyncio.run(main())
+
+        except KeyboardInterrupt:
+            #print(f'Run canceled on {datetime.now()}')
+            TIME_LOOP = False
+            conn.close()
