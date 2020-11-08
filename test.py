9301a406eb446cdb5988e37e3480bf0cbe5bccca
mario muuk <62722269+DrSuperMario@users.noreply.github.com>
2020-11-08 22:29:32 +0200

    update

--- test.py
+++ test.py
@@ -11,7 +11,6 @@ from smtp import send_email
 TIME_LOOP = True
 time_passage = 12000
 date_as = str(datetime.now())
-passwd = 'haxx0r001'
 
 conn5 = Connect.news(url=URL[4],header=HEADERS['agent_desktop'],reqToSend=True)
 conn = Connect.crypto(url=URL[1],header=HEADERS['agent_desktop'])
